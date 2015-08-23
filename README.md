29e5f5be1cdae5c4562af22e5cd95e3548425b34
Connor <connor_333@hotmail.co.uk>
2015-08-23 14:54:49 +0100

    Usage and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+** Status Icon Animator **
+
+- Creates a system tray icon that continually updates its icon according to the supplied frames. Making it appear as there is an animation in your system tray.
+
+** Example usage **
+
+`python status_icon_animator.py /home/USER/.icons/status_icons/my_frames/`
+Where the `my_frames` directory contains all the images that you'd like to display
--- status_icon_animator.py
+++ status_icon_animator.py
@@ -1,13 +1,31 @@
 """
 Overview
+     
+     This program allows for system tray icons to be animated, like a GIF is playing in
+	 the system tray. Any png/jpg/jpeg images can be used and displayed.
+
+Recommended usage
+     
+     The way I use this program is to find a GIF I'd like to display in the tray and then
+	 follow the steps below to split the GIF into seperate png/jpg files. This method doesn't
+	 have to be followed and any images can be used (don't have to be frames from a GIF)
 
 	 - to convert gif to png/jpg use: http://animizer.net/en/gif-apng-splitter
 	 - the output frames will have to the names frame-001.png, frame-002.png etc
 	 - to rename frame-001.png to frame-1.png (which is the required format) use the following command in the frames directory. Change .png to .jpg/.jpeg in the command if needed.
 	 - find -name '*.png' | awk 'BEGIN{ a=0 }{ printf "mv \"%s\" %s%d.png\n", $0, "frame-", a++ }' | bash
-	 - sometimes the command fails on the first run, if it does delete the output files and run the command again on a copy of the frames
+	 
+	 ! sometimes the command fails on the first run, if it does delete the output files and run the command again on a copy of the frames
+
+Requirements
+
+     - *nix OS 
+     - python 2
+     - the frames must have the file extension in their name
+	 - an absolute directory path must be used as the path_to_files argument
 """
 
+from sys import exit
 import gobject # for callbacks
 import gtk
 import argparse
@@ -41,37 +59,45 @@ class StatusIconAnimator():
         parser.add_argument("-t", "--time-per-frame", help="Specify the time for each frame to be shown in milliseconds. Default is 300ms.", type=int)
         args = parser.parse_args()
 
+        self.num_of_frames = len(fnmatch.filter(os.listdir(args.path_to_frames), self.frame_filename_prefix + "*" + self.frame_format)) # count number of frames in the supplied directory
+        if self.num_of_frames == 0:
+            print("Error: No suitable frames found in " + args.path_to_frames + "\nAborting...")
+            exit(-1)
+
         if args.time_per_frame:
             self.frame_time = args.time_per_frame
             if self.frame_time < 10:
-                print("Warning: A low time per frame value will lead to high CPU usage")
+                print("Warning: A low time per frame value will lead to high CPU usage.")
         if args.filetype == "jpg":
-		    self.frame_format = "." + args.filetype
+            self.frame_format = "." + args.filetype
         elif args.filetype == "jpeg":
-		    self.frame_format = "." + args.filetype
+            self.frame_format = "." + args.filetype
 
         frame_path_prefix = args.path_to_frames + self.frame_filename_prefix
-        self.num_of_frames = len(fnmatch.filter(os.listdir(args.path_to_frames), self.frame_filename_prefix + "*" + self.frame_format)) # count number of frames in the supplied directory
-
         for i in range(0, self.num_of_frames): # setup correct path names
-            self.frame_paths.append(frame_path_prefix + str(i) + self.frame_format)
+            self.frame_paths.append(frame_path_prefix + str(i + 1) + self.frame_format)
+
+        print(self.frame_paths)
+        if self.num_of_frames == 1: # we don't need callbacks if only one frame has been supplied so just set icon and return
+            self.icon.set_from_file(self.frame_paths[0])
+            return
 
         gobject.timeout_add(self.frame_time, self.update_frame) # callback from gtk.main()
         return
 
-
     def update_frame(self):
         """
         Changes the icons image to the next frame
+        when called via the callback
         """
 
         if self.frame_counter == self.num_of_frames: # otherwise potential overflow
-		    self.frame_counter = 0 
+            self.frame_counter = 0 
 		
         self.icon.set_from_file(self.frame_paths[self.frame_counter % self.num_of_frames]) # updates the icon with the next frame. Loops to start if at the last frame
 
         self.frame_counter += 1
-        return
+        return True
 
 if __name__=='__main__':
     animator = StatusIconAnimator()
